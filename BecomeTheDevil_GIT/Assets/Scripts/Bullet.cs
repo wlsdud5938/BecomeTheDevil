ef29a9997a0d6fe77704dc29d2c28f71ef890a46
shinseungyeol <shin10934@naver.com>
2018-10-06 15:23:01 +0900

    animator compleate

---
+++ BecomeTheDevil_GIT/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour{
+    public int bulletDamage = 20;
+    public const float bulletSpeed = 0.45f;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveY = bulletSpeed * Time.deltaTime;
+        //이동할 거리를 지정해 줍시다.
+        transform.Translate(0, moveY, 0);
+        //이동을 반영해줍시다.
+    }
+}
--- BecomeTheDevil_GIT/Assets/Scripts/Player.cs
+++ BecomeTheDevil_GIT/Assets/Scripts/Player.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Player : MovingUnit {
   
-    public int dir; // 0: front, 1: right, 2: back, 3: left
-    public float  movingSpeed = 0.01f;
-    private Animator animator;
-   
-    private enum Direction {FRONT,RIGHT,BACK,LEFT};
+
+    public float movingSpeed = 0.01f;
+    public GameObject bullet;
+
+
+    private int dir; // 0: front, 1: right, 2: back, 3: left
+    private bool isChop;
+    private enum Direction {FRONT,RIGHT,BACK,LEFT,FRONTLEFT,FRONTRIGHT,BACKRIGHT,BACKLEFT};
 	// Use this for initialization
+
+
+
+
+
 	protected override  void Start () {
         //animator = GetComponent<Animator>;
-        dir = -1;
+        dir = 0;
         GetComponent<Animator>().SetInteger("dir", dir);
+        GetComponent<Animator>().SetBool("isIdle", true);
         base.Start();
 
 		
 	}
 
-
     private int CalcDirection(int horizontal, int vertical){
+        if (vertical > 0 && horizontal > 0) return (int)Direction.FRONTRIGHT;
+        if (vertical > 0 && horizontal < 0) return (int)Direction.FRONTLEFT;
+        if (vertical < 0 && horizontal > 0) return (int)Direction.BACKRIGHT;
+        if (vertical < 0 && horizontal < 0) return (int)Direction.BACKLEFT;
         if (vertical > 0) return (int)Direction.FRONT;
         if (vertical < 0) return (int)Direction.BACK;
         if (horizontal > 0) return (int)Direction.RIGHT;
@@ -33,25 +45,35 @@ public class Player : MovingUnit {
 	void Update () {
         int horizontal = 0;
         int vertical = 0;
-        dir = -1;
-        GetComponent<Animator>().SetInteger("dir", dir);
+        isChop = false;
+        GetComponent<Animator>().SetBool("isChop", isChop);
+        GetComponent<Animator>().SetBool("isIdle", true);
 
         horizontal = (int)Input.GetAxisRaw("Horizontal"); 
         vertical = (int)Input.GetAxisRaw("Vertical");
 
-        if (horizontal != 0){
-            vertical = 0;
-        }
- 
-        if(horizontal!=0 || vertical!=0){
+    
+        if (horizontal != 0 || vertical != 0)
+        {
+
             dir = CalcDirection(horizontal, vertical);
             GetComponent<Animator>().SetInteger("dir", dir);
-            AttemptMove(horizontal*movingSpeed, vertical*movingSpeed);
-        
+            GetComponent<Animator>().SetBool("isIdle", false);
+            AttemptMove(horizontal * movingSpeed, vertical * movingSpeed);
+
         }
-
-
-
+        if (Input.GetMouseButton(0))
+        {
+            isChop = true;
+            GetComponent<Animator>().SetBool("isIdle", false);
+            GetComponent<Animator>().SetBool("isChop", isChop);
+            Fire();
+        }
+    }
+   
+    void Fire()
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
     }
 
 
