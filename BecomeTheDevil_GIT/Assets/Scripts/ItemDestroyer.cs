f5444c24a677953e1717cdc6f9fee63996c6cee6
wlsdud5938 <wlsduddud23@gmail.com>
2018-10-14 03:10:12 +0900

    15분마다 랜덤 위치에 아이템 생성 및 3분뒤 키 아이템 랜덤생성

---
+++ BecomeTheDevil_GIT/Assets/Scripts/ItemDestroyer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+    public float timer = 0;
+    private RoomTemplates templates;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        if (timer >= 15.0f && gameObject.tag == "Item")
+        {
+            Destroy(gameObject);
+            templates.spawnedItem = false;
+        }
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (gameObject.tag == "Item")
+                templates.spawnedItem = false;
+            if (gameObject.tag == "KeyItem")
+                templates.spawnedKeyItem = false;
+        }
+        Destroy(gameObject);
+    }
+}
--- BecomeTheDevil_GIT/Assets/Scripts/RoomTemplates.cs
+++ BecomeTheDevil_GIT/Assets/Scripts/RoomTemplates.cs
@@ -5,18 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class RoomTemplates : MonoBehaviour
 {
-
+    public List<Transform> centerPoints;
     public GameObject[] bottomRooms;
     public GameObject[] topRooms;
     public GameObject[] leftRooms;
     public GameObject[] rightRooms;
     public static int roomCount;
     public GameObject closedRoom;
+    public GameObject items;
+    public GameObject keyItems;
+    float timer = 0;
 
     public List<GameObject> rooms;
 
     public float waitTime;
     private bool spawnedBoss;
+    public bool spawnedKeyItem;
+    public bool spawnedItem;
     public GameObject boss;
     void Awake()
     {
@@ -24,7 +29,7 @@ public class RoomTemplates : MonoBehaviour
     }
     void Update()
     {
-
+        timer += Time.deltaTime;
         if (waitTime <= 0 && spawnedBoss == false)
         {
             for (int i = 0; i < rooms.Count; i++)
@@ -33,14 +38,31 @@ public class RoomTemplates : MonoBehaviour
                 {
                     if (rooms.Count <= 8)
                         SceneManager.LoadScene("ChoiJinyoung");
-                    Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
-                    spawnedBoss = true;
+                    else
+                    {
+                        Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                        spawnedBoss = true;
+                    }
                 }
             }
         }
         else
         {
             waitTime -= Time.deltaTime;
+        }
+        if(spawnedBoss == true && spawnedItem==false)
+        {
+            int targetIndex = Random.Range(1, rooms.Count-2);
+            Instantiate(items, rooms[targetIndex].transform.position, Quaternion.identity);
+
+            spawnedItem = true;
+        }
+        if (spawnedBoss == true && spawnedKeyItem == false && timer > 180.0f)
+        {
+            int targetIndex = Random.Range(1, rooms.Count - 2);
+            Instantiate(keyItems, rooms[targetIndex].transform.position, Quaternion.identity);
+
+            spawnedKeyItem = true;
         }
     }
 }
