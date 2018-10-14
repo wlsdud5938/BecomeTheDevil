831b990e467b7180452025f6db020811bd1f3089
wlsdud5938 <wlsduddud23@gmail.com>
2018-10-15 01:26:13 +0900

    성철씨 수정

--- BecomeTheDevil_GIT/Assets/Scripts/Enemy.cs
+++ BecomeTheDevil_GIT/Assets/Scripts/Enemy.cs
@@ -8,15 +8,25 @@ public class Enemy : MonoBehaviour
 
     public float moveSpeed = 1.0f; // 이동속도
     public float attackRange = 1.0f; // 무기의 Range, 작을 수록 가까이 달라붙습니다.
+    public float obstacleRange = 1.0f; // 벽에 부딪혔다고 판정하는 거리 기준입니다.
+    public float AiRange = 1.0f; // AI의 Player, Tower를 찾는 거리입니다. 이 안에선 Player와 Tower를 우선 공격합니다. 그 외에선 core를 찾습니다.
+    public float AiTime = 1.0f; // AI가 경로를 수정하는 시간을 조절합니다.
+    
     public float distanceOfTile = 1.0f; // 애니메이터를 좌,우 -> 상 하로 전환시킬 최소한의 거리입니다. 
     public float hp = 100f;
     private Transform target; // 쫓아갈 대상
+    private Transform wall; // 근처의 벽
+    private Transform center; // 근처의 중앙점
+    public float dist; // AI관련 거리변수
     private Animator animation; // Animator 선언
 
     private BoxCollider2D boxCollider;
     private Rigidbody2D rigidbodys; // test
 
+    //private RoomTemplates templates; //방 생성 알고리즘을 불러오는 부분입니다.
+
     private float adv; //enemy와 player사이의 distance입니다.
+    private float odv; // obstacle과 enemy 사이의 distance입니다.
 
     private int dir; // animator의 방향인식 int값입니다. 아래에 상술.
     //Animator에서, Int0 = Front / 1 = Back / 2 = Left / 3 = Right / 4 = None
@@ -37,9 +47,12 @@ public class Enemy : MonoBehaviour
     // 18.10.14 Start() -> Awake(), 김윤성 Awake로 바꾸니까 에러뜨네요. 다시 Start로 수정했습니다.
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         //boxCollider.GetComponent<BoxCollider2D>();
         animation = GetComponent<Animator>();
+        //templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>(); // 방 생성 알고리즘 불러오는 부분입니다.
+        InvokeRepeating("getClosestEnemy", 0, AiTime);
+        InvokeRepeating("getClosestCenter", 0, (AiTime+1));
+        InvokeRepeating("getClosestWall", 0, 1);
 
         // 김윤성이 추가했습니다.
         IsActive = true;
@@ -53,11 +66,27 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        //wall = GameObject.FindGameObjectWithTag("Wall").GetComponent<Transform>();
         Vector2 targetPos = target.position;
         Vector2 myPos = transform.position;
+        Vector2 wallPos = wall.position;
+        Vector2 centerPos = center.position;
         adv = Vector2.Distance(transform.position, target.position); // adv에 값을 넣어줍니다.
+        odv = Vector2.Distance(transform.position, wall.position);
+
+        if (target != null)//debug용
+        {
+            Debug.DrawLine(transform.position, target.position,
+                           Color.green); // debug용.
+        }
+
+        if ((odv <= obstacleRange) && (adv > attackRange))// odv(벽과 enemy의 거리)가 지정 거리보다 작으면 벽을 벗어나려합니다. 주변에 공격할 대상이 없어야합니다.
+        {
+            moveEnemy(centerPos, myPos, moveSpeed, adv);
+        }
         
-        if (adv >= attackRange) // Range보다 멀리 있으므로 Run해야합니다.
+        if ((adv >= attackRange) && (odv > obstacleRange)) // Range보다 멀리 있으므로 Run해야합니다.
         {
             if (targetPos.x <= myPos.x) //플레이어가 Enemy(자신)보다 좌측이면,
             {
@@ -184,6 +213,78 @@ public class Enemy : MonoBehaviour
         rigidbodys.position = Vector2.MoveTowards(ypos, xpos, speed * Time.deltaTime);
     }
 
+
+    void getClosestEnemy()
+    {
+        //비용이 큼 - 특정 태그의 오브젝트를 모두 찾음
+        GameObject[] taggedEnemys = GameObject.FindGameObjectsWithTag("Player");
+        float closestDistSqr = Mathf.Infinity;//infinity 실제값?
+        Transform closest = null;
+        foreach (GameObject taggedEnemy in taggedEnemys)
+        {
+            Vector3 objectPos = taggedEnemy.transform.position;
+            dist = (objectPos - transform.position).sqrMagnitude;
+            // 특정 거리 안으로 들어올때
+            if (dist < AiRange+5000)
+            {
+                // 그 거리가 제곱한 최단 거리보다 작으면
+                if (dist < closestDistSqr)
+                {
+                    closestDistSqr = dist;
+                    closest = taggedEnemy.transform;
+                }
+            }
+        }
+        target = closest;
+    }
+
+    void getClosestCenter()
+    {
+        //비용이 큼 - 특정 태그의 오브젝트를 모두 찾음
+        GameObject[] taggedcenters = GameObject.FindGameObjectsWithTag("CenterPoint");
+        float closestDistSqr = Mathf.Infinity;//infinity 실제값?
+        Transform closest = null;
+        foreach (GameObject taggedEnemy in taggedcenters)
+        {
+            Vector3 objectPos = taggedEnemy.transform.position;
+            dist = (objectPos - transform.position).sqrMagnitude;
+            // 특정 거리 안으로 들어올때
+            if (dist < AiRange+500)
+            {
+                // 그 거리가 제곱한 최단 거리보다 작으면
+                if (dist < closestDistSqr)
+                {
+                    closestDistSqr = dist;
+                    closest = taggedEnemy.transform;
+                }
+            }
+        }
+        center = closest;
+    }
+
+    void getClosestWall()
+    {
+        //비용이 큼 - 특정 태그의 오브젝트를 모두 찾음
+        GameObject[] taggedwalls = GameObject.FindGameObjectsWithTag("Wall");
+        float closestDistSqr = Mathf.Infinity;//infinity 실제값?
+        Transform closest = null;
+        foreach (GameObject taggedEnemy in taggedwalls)
+        {
+            Vector3 objectPos = taggedEnemy.transform.position;
+            dist = (objectPos - transform.position).sqrMagnitude;
+            // 특정 거리 안으로 들어올때
+            if (dist < AiRange+3)
+            {
+                // 그 거리가 제곱한 최단 거리보다 작으면
+                if (dist < closestDistSqr)
+                {
+                    closestDistSqr = dist;
+                    closest = taggedEnemy.transform;
+                }
+            }
+        }
+        wall = closest;
+    }
 
     // 김윤성이 추가
     public void TakeDamage(int damage)
