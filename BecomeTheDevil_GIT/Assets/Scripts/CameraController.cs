0a3364028ab0c841fe7a458aefdebc6f19b699c9
wlsdud5938 <wlsduddud23@gmail.com>
2018-10-15 00:40:50 +0900

    유닛 생성
    일단 왼쪽에만 생성

--- BecomeTheDevil_GIT/Assets/Scripts/CameraController.cs
+++ BecomeTheDevil_GIT/Assets/Scripts/CameraController.cs
@@ -12,7 +12,7 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.M) || Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.M))
         {
             //don't forget to set one as active either in the Start() method 
             //or deactivate 1 camera in the Editor before playing 
---
+++ BecomeTheDevil_GIT/Assets/Scripts/UnitCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitCreator : MonoBehaviour {
+    private Player player;
+    private float timer = 10.0f;
+    public Vector3 playerPosition;
+    public GameObject unit;
+    public bool a = true;
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != GameObject.FindGameObjectWithTag("Player"))
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            playerPosition = player.gameObject.transform.position;
+            timer += Time.deltaTime;
+        }
+    }
+
+    public void OnClick()
+    {
+        if(timer >= 10.0f)
+        {
+            Instantiate(unit, player.transform.position, Quaternion.identity);
+        }
+    }
+
+}
