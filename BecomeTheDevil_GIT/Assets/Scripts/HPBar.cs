5848f7a7cb986fbeccb029d14e1ede19777b39bc
KamJaTang <43845624+KamJaTang@users.noreply.github.com>
2018-11-06 01:55:17 +0900

    HPBar 추가!

---
+++ BecomeTheDevil_GIT/Assets/Scripts/HPBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    public Slider HPSlider;
+    private Statu stat;
+
+    // Use this for initialization
+    void Start () {
+        stat = gameObject.GetComponent<Statu>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateHPBar();
+	}
+
+    void UpdateHPBar()
+    {
+        HPSlider.maxValue = stat.HP;
+        HPSlider.value = stat.currentHP;
+        
+        // statu의 값을 받아와 HP Silder의 값을 변경한다.
+    }
+}
--- BecomeTheDevil_GIT/Assets/Scripts/Statu.cs
+++ BecomeTheDevil_GIT/Assets/Scripts/Statu.cs
@@ -8,16 +8,15 @@ public class Statu : MonoBehaviour {
     public float attackRange = 10f;   //공격 사거리
     public float attackSpeed = 1f;   //공격 속도
     public float HP = 100;            //최대 hp
-    public float currentHP;     //현재 hp
+    public float currentHP;            //현재 hp
 
 	// Use this for initialization
 	void Start () {
-        currentHP = HP;
+        currentHP = 60;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     public void TakeDamage(float otherDamage)
