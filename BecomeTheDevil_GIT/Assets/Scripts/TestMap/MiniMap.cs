5fcea1b8a9384aef07c1e15993a7462d7cf582c7
wlsdud5938 <wlsduddud23@gmail.com>
2018-11-13 04:19:58 +0900

    minimap 구현

---
+++ BecomeTheDevil_GIT/Assets/Scripts/TestMap/MiniMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    // Use this for initialization
+    private bool isOn = false;
+    private RoomTemplates templates;
+
+
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOn == false && templates.spawnedBoss == true)
+        {
+            transform.Find("MiniMapCam").gameObject.SetActive(true);
+            isOn = true;
+        }
+    }
+}
