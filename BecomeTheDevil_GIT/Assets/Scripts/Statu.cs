31711cd92040fc0805d577b3d65a52cb61ecd9d7
wlsdud5938 <wlsduddud23@gmail.com>
2018-11-24 14:44:18 +0900

    플레이어 벽 통과하는 버그 수정 완료

--- BecomeTheDevil_GIT/Assets/Scripts/Statu.cs
+++ BecomeTheDevil_GIT/Assets/Scripts/Statu.cs
@@ -38,8 +38,8 @@ public class Statu : MonoBehaviour {
         }//에너미인 경우, 웨이브 라운드에 숫자에 따라 강해짐 최대체력이 달라짐
 
         currentHP = maxHP;          // 처음 생성됐을 때 최대 체력을 갖고 태어남.
-        HPSlider.maxValue = maxHP;
-        HPSlider.value = currentHP;
+        //HPSlider.maxValue = maxHP;
+        //HPSlider.value = currentHP;
     }
 	
 	// Update is called once per frame
