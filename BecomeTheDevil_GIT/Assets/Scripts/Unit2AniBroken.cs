0e33b1713ca190c5d1014b7b8fec7308cc9ba65f
SungChul Kim <42808923+wavyfire@users.noreply.github.com>
2018-12-02 23:38:51 +0900

    Unit 1 2 3 추가 및 수정, 애니메이션 수정, 스크립트 두개 추가
    추가된 스크립트 UnitPumping.cs , Unit2AniBroken.cs 입니다.
    간단한 내용으로 되어있어요.
    
    유닛 2에는 AniBroken 스크립트가,
    유닛 1,3에는 UnitPumping이 되어있습니다.
    
    애니메이션 약간 수정 하였고, 프리팹들의 콜라이더도 조정 되었습니다.

---
+++ BecomeTheDevil_GIT/Assets/Scripts/Unit2AniBroken.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit2AniBroken : MonoBehaviour {
+    private Animator animator;
+    Statu statu;
+    public float percent; // 애니메이션 변화를 위한 기준치 설정 (*4, *3, *2, *1이 되므로 가장 부서진 상태의 에니메이션이 되는 수치를 입력)
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        statu = GetComponent<Statu>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.statu.currentHP >= (percent*3))
+        {
+            animator.SetFloat("Hp", 0); // 0, 0.3, 0.6, 1 : 1로 갈 수록 부서진 애니메이션.
+        }
+        else if (this.statu.currentHP >= (percent * 2) && (this.statu.currentHP < (percent * 3)))
+        {
+            animator.SetFloat("Hp", 0.33f); // 0, 0.3, 0.6, 1 : 1로 갈 수록 부서진 애니메이션.
+        }
+        else if (this.statu.currentHP >= (percent * 1) && (this.statu.currentHP < (percent * 2)))
+        {
+            animator.SetFloat("Hp", 0.66f); // 0, 0.3, 0.6, 1 : 1로 갈 수록 부서진 애니메이션.
+        }
+        else if (this.statu.currentHP >= (percent * 0) && (this.statu.currentHP < (percent * 1)))
+        {
+            animator.SetFloat("Hp", 1); // 0, 0.3, 0.6, 1 : 1로 갈 수록 부서진 애니메이션.
+        }
+    }
+}
---
+++ BecomeTheDevil_GIT/Assets/Scripts/UnitPumping.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPumping : MonoBehaviour {
+    private Animator animator;
+    Statu statu;
+    private int rand;
+    public int prequency; // 펌핑이 발생할 주기. 1~100까지이며 큰 수치 = 자주 발생.
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        statu = GetComponent<Statu>();
+        animator.SetTrigger("Pump"); //처음 한 번 실행.
+        InvokeRepeating("Pumping", 0, 5.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private void Pumping()
+    {
+        rand = Random.Range(0, 100);
+        if (rand >= prequency)
+        {
+            animator.SetTrigger("Pump");
+        }
+            
+    }
+}
