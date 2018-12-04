a07de71b63f0c22a5805f78c957b08c6263b4dc9
SungChul Kim <42808923+wavyfire@users.noreply.github.com>
2018-12-05 00:56:03 +0900

    Sound 목록 갱신, Enemy, Player FootStep Sound 추가
    Enemy는 아무래도 빼야할 것 같습니다.

---
+++ BecomeTheDevil_GIT/Assets/Scripts/Sound/EnemyMoveSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveSound : MonoBehaviour
+{
+    public AudioSource audio1;
+    public AudioClip audio;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        audio1 = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        audio1.Play();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (animator.GetBool("EnemyRun") == true)
+        {
+            audio1.mute = false;
+        }
+        if (animator.GetBool("EnemyRun") == false)
+        {
+            audio1.mute = true;
+        }
+    }
+}
